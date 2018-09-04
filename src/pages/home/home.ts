d5ca4e86182fd2137eaf8dcb5c8d161b7ce436de
Murtaza <murtaza.dev@gmail.com>
2018-09-04 15:35:00 +0500

    Init Push

---
+++ src/pages/home/home.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+// ForceJS Plugin
+import { OAuth, DataService } from 'forcejs';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  users: any;
+  constructor(public navCtrl: NavController) {
+    this.loadUsers()
+  }
+
+  loadUsers() {
+    let oauth = OAuth.createInstance();
+    oauth.login()
+      .then(oauthResult => {
+        let service = DataService.createInstance(oauthResult);
+        service.query('SELECT Id, Name FROM User LIMIT 10')
+          .then(response => {
+            this.users = response.records;
+          });
+      });
+  }
+}
